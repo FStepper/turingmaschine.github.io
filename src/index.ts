ae32fcb4eeea0e87975dcd397f9dc86d4997cf6d
Yeomans, Vincent <vincent.yeomans@study.thws.de>
2023-03-30 21:06:17 +0200

    crated index.ts and index.html

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { app, BrowserWindow } from "electron";
+//import * as path from "path";
+
+app.on("ready", () => {
+  console.log("App is ready");
+
+  const win = new BrowserWindow({
+    width: 600,
+    height: 400,
+  });
+
+  //const indexHTML = path.join(__dirname + "/");
+  win
+    .loadFile("dist/index.html")
+    .then(() => {
+        console.log('index.html loadad');
+    })
+    .catch((e) => console.error(e));
+});
